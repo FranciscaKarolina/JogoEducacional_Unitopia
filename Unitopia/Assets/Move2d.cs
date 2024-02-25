68fcf7da5a02a0645cb20389a9854157ff79a7b6
Linaqueiroz <queirozkarolina30@gmail.com>
2024-02-25 05:22:23 -0300

    Alteração no personagem jogador
    Inserção de movimentos, pulo e connfigurações

--- Unitopia/Assets/Move2d.cs
+++ Unitopia/Assets/Move2d.cs
@@ -8,13 +8,40 @@ public class Move2d : MonoBehaviour
     public int moveSpeed;
     private float direction;
 
+    private Vector3 facinRight;
+    private Vector3 facinLeft;
+
+    public bool taNoChao;
+    public Transform detectaChao;
+    public LayerMask oQueEhChao;
+
     void Start()
     {
+        //Personagem virar para a direita//
+        facinRight = transform.localScale;
+        //Personagem virar para a direita//
+        facinLeft = transform.localScale;
+        facinLeft.x = facinLeft.x * -1; //O comando inverte os sinais, por isso o uso de *
         rb = GetComponent<Rigidbody2D>(); 
     }
     void Update()
-    {
+    { 
+        taNoChao = Physics2D.OverlapCircle(detectaChao.position, 0.2f, oQueEhChao);
+        if(Input.GetButtonDown("Jump") && taNoChao == true)
+        {
+            rb.velocity = Vector2.up * 12;
+        }
+
         direction = Input.GetAxis("Horizontal");
+        if(direction > 0){
+            //olhando para a direita
+            transform.localScale = facinRight;
+        }
+        if(direction < 0){
+            //olhando para a esquerda
+            transform.localScale = facinLeft;
+        }
+
         rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
     }
 }
