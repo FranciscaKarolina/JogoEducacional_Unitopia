5a43712fe4c144a42965628bd40d0d855bd8f95e
Karolina Queiroz <99521034+FranciscaKarolina@users.noreply.github.com>
2024-02-27 21:59:29 -0400

    Menu e coletaveis
    Desenvolvimento de tela de menu, colisão para um objeto coletável

---
+++ Unitopia/Assets/LoadScenes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    //Menu
+    public void MenuScene(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Credits(){
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
