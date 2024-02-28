eb4ed9442514dc7e625a5a1bb9be749ba07543e9
Karolina Queiroz <99521034+FranciscaKarolina@users.noreply.github.com>
2024-02-28 17:51:02 -0400

    Tela de níveis e menu
    Criando tela de niveis e menu e fazendo a correspondencia

--- Unitopia/Assets/LoadScenes.cs
+++ Unitopia/Assets/LoadScenes.cs
@@ -10,17 +10,31 @@ public class LoadScenes : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    public void Credits(){
-        
-    }
-    void Start()
+    public void Options()
     {
-        
+        SceneManager.LoadScene("Options");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Creditos()
     {
-        
+        SceneManager.LoadScene("Creditos");
+    }
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene("SelectLevel");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    //ALL LEVELS
+    public void Fase1(){
+        SceneManager.LoadScene("Fase1");
+    }
+    public void Fase2(){
+        SceneManager.LoadScene("Fase2");
+    }
+    public void Fase3(){
+        SceneManager.LoadScene("Fase3");
     }
 }
